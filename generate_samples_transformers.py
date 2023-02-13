ec52759c8e4039240b16b445071cae05e2ada91e
BoyuanJackChen <bc3194@nyu.edu>
2023-02-12 21:36:22 -0500

    all softmax thresholds

--- generate_samples_transformers.py
+++ generate_samples_transformers.py
@@ -6,7 +6,7 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--loaded_model', type=str, default='Salesforce/codegen-16B-mono')
+parser.add_argument('--loaded_model', type=str, default='Salesforce/codegen-350M-mono')
 parser.add_argument('--device', type=str, default='cuda:0')
 parser.add_argument('--num_samples_per_task', type=int, default=1)
 parser.add_argument('--beam_width', type=int, default=4)
@@ -44,16 +44,16 @@ def main(args):
     model = AutoModelForCausalLM.from_pretrained(loaded, device_map="auto")
     if args.early_exit_layer is not None:
         model.set_early_exit_layer(args.early_exit_layer)
-    softmax_threshold = 0.95
-    model.set_softmax_threshold(softmax_threshold)
 
     beam_width = args.beam_width
     num_beam_groups = args.num_beam_groups
     beam_diversity_rate = args.beam_diversity_rate
     all_beam_widths = [4]
     all_diversity = [0.3]
+    softmax_thresholds = [0.95, 0.99, 0.9, 0.8]
+    all_exit_layers = np.array([])
 
-    def generate_one_completion(prompt):
+    def generate_one_completion(prompt, all_exit_layers):
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
         generated_ids = model.generate(
             input_ids.to('cuda'),
@@ -65,30 +65,39 @@ def main(args):
             diversity_penalty=beam_diversity_rate,
         )
         generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-        model.clear_early_exit_layer_indices()
+        early_layers = model.get_early_exit_layer_indices()
+        early_layers = early_layers[:early_layers.count_nonzero()].numpy()
+        all_exit_layers = np.append(all_exit_layers, early_layers)
         # print(f"generated_text is:\n{generated_text}")
         trimmed_text = trim_with_stopwords(generated_text, stop_words, prompt)
         print(f"trimmed_text is:\n{trimmed_text}")
-        return trimmed_text
+        model.clear_early_exit_layer_indices()
+        return trimmed_text, all_exit_layers
 
-    for beam_width in all_beam_widths:
-        for beam_diversity_rate in all_diversity:
-            num_beam_groups = beam_width
-            print(f"samples started on {beam_width, beam_diversity_rate}")
-            samples = []
-            for task_id in problems:
-                print(f"Generating task {task_id}")
-                for _ in range(num_samples_per_task):
-                    sample = dict(
-                        task_id=task_id,
-                        completion=generate_one_completion(problems[task_id]["prompt"])
-                    )
-                    samples.append(sample)
+    for softmax_threshold in softmax_thresholds:
+        model.set_softmax_threshold(softmax_threshold)
+        for beam_width in all_beam_widths:
+            for beam_diversity_rate in all_diversity:
+                num_beam_groups = beam_width
+                print(f"samples started on {beam_width, beam_diversity_rate, softmax_threshold}")
+                samples = []
+                for task_id in problems:
+                    print(f"Generating task {task_id}")
+                    for _ in range(num_samples_per_task):
+                        completion, all_exit_layers = generate_one_completion(problems[task_id]["prompt"], all_exit_layers)
+                        sample = dict(
+                            task_id=task_id,
+                            completion=completion
+                        )
+                        samples.append(sample)
+                        print(all_exit_layers)
 
-            filename = f"softmax_{softmax_threshold}_{beam_width}_{num_beam_groups}_{beam_diversity_rate}_transformers{model_name}"
-            if args.early_exit_layer is not None:
-                filename += f"_early{args.early_exit_layer}"
-            write_jsonl(filename + ".jsonl", samples)
+                filename = f"softmax_{softmax_threshold}_{beam_width}_{num_beam_groups}_{beam_diversity_rate}_transformers{model_name}"
+                if args.early_exit_layer is not None:
+                    filename += f"_early{args.early_exit_layer}"
+                write_jsonl(filename + ".jsonl", samples)
+                np.save(filename + "_early_layers.npy", all_exit_layers)
+                print(np.mean(all_exit_layers))
 
     # print("samples started")
     # samples = []
